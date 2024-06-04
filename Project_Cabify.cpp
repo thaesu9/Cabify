b886a589358deb5875313783a1827e565ec5577d
thaesu9 <blossomthaesu@gmail.com>
2024-06-05 01:08:30 +1200

    logo+update

--- Project_Cabify.cpp
+++ Project_Cabify.cpp
@@ -8,7 +8,7 @@
 #include <cstdlib>
 using namespace std;
 
-int main()
+void CabifyLogo()
 {
     cout << "                                                                     " << endl;
     cout << "                              ========                               " << endl;
@@ -22,6 +22,51 @@ int main()
     cout << "          /_____|      |____________________|      |____\\           " << endl;
     cout << "                |______|                    |______|                 " << endl;
     cout << "=====================================================================" << endl << endl;
+}
+
+int main()
+{
+    CabifyLogo();
+
     cout << "                         Welcome to CABIFY!                          " << endl;
+
+    cout << "This is Cabify, your Taxi service to make your journey pleasurable..." << endl;
+
+    cout << "Please choose ONE option from below." << endl;
+    cout << "(By making a choice, you agree to our terms and conditions.)" << endl << endl;
+    cout << "1. Book a Cab. (Customer)" << endl;
+    cout << "2. Drive for Cabify. (Driver)" << endl;
+    cout << "3. Check Terms and Conditions." << endl;
+    cout << "4. Exit." << endl;
+
+    int choice;
+    cout << "\nChoice: ";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        cout << endl;
+        break;
+
+    case 2:
+        cout << endl;
+        break;
+
+    case 3:
+        cout << endl;
+        break;
+
+    case 4:
+        cout << endl;
+        return 0;
+
+    default:
+        cout << "Invalid choice. Please choose again : ";
+        break;
+    }
+
+
+
 }
 
