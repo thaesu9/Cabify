5b7a5052ac76860c015a66e6cb422447fa91f3dc
thaesu9 <blossomthaesu@gmail.com>
2024-06-05 00:47:31 +1200

    logo

---
+++ Project_Cabify.cpp
@@ -0,0 +1,27 @@
+// Project_Cabify.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    cout << "                                                                     " << endl;
+    cout << "                              ========                               " << endl;
+    cout << "                          ___/________\\___                           " << endl;
+    cout << "                         /                \\                          " << endl;
+    cout << "               _________/                  \\________                " << endl;
+    cout << "              /       __   _   __  ___  __ _  _     \\               " << endl;
+    cout << "             /      //    /_\\ |__|  |  |__ \\\\//      \\              " << endl;
+    cout << "            /       \\\\__ /   \\|__| _|_ |    ||        \\             " << endl;
+    cout << "           /     ______                      ______    \\            " << endl;
+    cout << "          /_____|      |____________________|      |____\\           " << endl;
+    cout << "                |______|                    |______|                 " << endl;
+    cout << "=====================================================================" << endl << endl;
+    cout << "                         Welcome to CABIFY!                          " << endl;
+}
+
